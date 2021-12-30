b01312c93e0a6d2874a4fccf4a2a540f15b3e3df
Jesse Maarleveld <jessemaarleveld@gmail.com>
2021-12-30 22:04:46 +0100

    Add better handling of markup in text cleaning

--- code/text_cleaner.py
+++ code/text_cleaner.py
@@ -11,6 +11,7 @@ in NLP applications.
 
 import re
 import string
+import typing
 
 import gensim.downloader
 import nltk.corpus
@@ -42,7 +43,7 @@ replacements = {
 ###############################################################################
 
 
-def normalize_text(text: str) -> list[str]:
+def normalize_text(text: str) -> typing.List[str]:
     text = remove_formatting(text)
     sentences = nltk.tokenize.sent_tokenize(text)
     return [
@@ -52,10 +53,17 @@ def normalize_text(text: str) -> list[str]:
 
 
 def remove_formatting(text: str) -> str:
-    text = re.sub(r'\<.*?\>', '', text)
     text = re.sub(r'\[.*?\|.*?\]', 'LLLINK', text)
+    text = re.sub(r'\[.*?\]', 'LLLINK', text)
+    text = re.sub(r'\{code.*?\}(.|\s)*?\{code\}', 'CCCODEBLOCK', text)
     text = re.sub(r'\{\{.*?\}\}', 'IIINLINECODE', text)
-    text = re.sub(r'@[^\s]+', 'MMMENTION', text)
+    text = re.sub(r'\[\~[^\s]+\]', 'MMMENTION', text)
+    text = re.sub(r'\{.*?\}', '', text)
+    text = re.sub(r'\!.*?\!', 'GGGRAPHIC', text)
+    text = re.sub(r'h[1-6]\.', '', text)
+    text = re.sub(r'\|', ' | ', text)
+    text = re.sub(r'bq\.', '', text)
+    text = re.sub(r'\<.*?\>', '', text)
     return text
 
 
@@ -86,12 +94,117 @@ def normalize_sentence(text: str) -> str:
     return ' '.join(words)
 
 
-with open('output.json') as file:
-    import json
-    data = json.load(file)
 
-for item in data:
-    print(repr(item['comments']))
-    print(normalize_text(item['comments']))
-    raise
+format_test = """
+h1. Biggest heading
+h2. Bigger heading
+h3. Big heading
+h4. Normal heading
+h5. Small heading
+h6. Smallest heading
+*strong*
+_emphasis_
+??citation??
+-deleted-
++inserted+
+^superscript^
+~subscript~
+{{monospaced}}
+bq. Some block quoted text
+{quote}
+    here is quotable
+ content to be quoted
+{quote}
+{color:red}
+    look ma, red text!
+{color}
+----
+[#anchor]
+[^attachment.ext]
+[http://jira.atlassian.com]
+[Atlassian|http://atlassian.com]
+[mailto:legendaryservice@atlassian.com]
+[file:///c:/temp/foo.txt]
+[file:///z:/file/on/network/share.txt]
+{anchor:anchorname}
+[~username]
+* some
+* bullet
+** indented
+** bullets
+* points
 
+- different
+- bullet
+- types
+
+# a
+# numbered
+
+# list
+
+# a
+# numbered
+#* with
+#* nested
+#* bullet
+# list
+
+* a
+* bulleted
+*# with
+*# nested
+*# numbered
+* list
+
+!http://www.host.com/image.gif!
+or
+!attached-image.gif!
+
+!image.jpg|thumbnail!
+
+!image.gif|align=right, vspace=4!
+
+!quicktime.mov!
+!spaceKey:pageTitle^attachment.mov!
+!quicktime.mov|width=300,height=400!
+!media.wmv|id=media!
+
+||heading 1||heading 2||heading 3||
+|col A1|col A2|col A3|
+|col B1|col B2|col B3|
+
+{noformat}
+preformatted piece of text
+ so *no* further _formatting_ is done here
+{noformat}
+
+{panel}
+Some text
+{panel}
+
+{panel:title=My Title}
+Some text with a title
+{panel}
+
+{panel:title=My Title|borderStyle=dashed|borderColor=#ccc|titleBGColor=#F7D6C1|bgColor=#FFFFCE}
+a block of text surrounded with a *panel*
+yet _another_ line
+{panel}
+
+{code:title=Bar.java|borderStyle=solid}
+// Some comments here
+public String getFoo()
+{
+    return foo;
+}
+{code}
+
+{code:xml}
+    <test>
+        <another tag="attribute"/>
+    </test>
+{code}
+"""
+
+print(normalize_text(format_test))
