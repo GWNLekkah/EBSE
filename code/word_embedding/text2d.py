010a67da71cf914a5fd15cdd6ff790b500a6ee10
GWNLekkah <a.j.dekker.5@student.rug.nl>
2022-01-19 00:56:46 +0100

    Description only vocab + min word frequency of 5

--- code/word_embedding/text2d.py
+++ code/word_embedding/text2d.py
@@ -16,8 +16,8 @@ def main():
     with open('../issuedata_extractor/non_architectural_issues.json') as file:
         issues_dict += json.load(file)
 
-    str_keys = ['summary', 'description']
-    list_keys = ['comment_list']
+    str_keys = ['description']
+    list_keys = []
 
     all_tokens = []
     with alive_bar(len(issues_dict)) as bar:
@@ -33,18 +33,26 @@ def main():
             all_tokens.append(issue_text)
             bar()
 
-    max_len = max([len(tokens) for tokens in all_tokens])
+    # Limit the range to 1000
+    max_len = 1000
+    for idx in range(len(all_tokens)):
+        if len(all_tokens[idx]) > 1000:
+            all_tokens[idx] = all_tokens[idx][0:1000]
 
-    model = Word2Vec(all_tokens, min_count=1, vector_size=5)
+    print('training')
+    vector_size = 8
+    model = Word2Vec(all_tokens, min_count=8, vector_size=vector_size)
     model.wv.save_word2vec_format('word2vec.txt', binary=False)
+    print('done training')
 
     issue_matrices = []
     with alive_bar(len(all_tokens)) as bar:
         for tokens in all_tokens:
             issue_matrix = []
             for token in tokens:
-                issue_matrix.append(model.wv[token].tolist())
-            issue_matrix.extend([[0] * 5] * (max_len - len(issue_matrix)))
+                if token in model.wv.key_to_index:
+                    issue_matrix.append(model.wv[token].tolist())
+            issue_matrix.extend([[0] * vector_size] * (max_len - len(issue_matrix)))
             issue_matrices.append(issue_matrix)
             bar()
 
